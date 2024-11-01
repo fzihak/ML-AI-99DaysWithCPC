99c333ef2fbd1216e8388e5f3caadb0985c3829f
Foysal Zihak <67580891+fzihak@users.noreply.github.com>
2024-11-01 23:42:19 +0600

    Day-1

---
+++ Day-1/day-1.py
@@ -0,0 +1,24 @@
+print("Hello World!")
+import sys
+
+print(sys.version)
+if 5 > 2:
+  print("Five is greater than two!")
+
+x = 5
+y = "Hello, World!"
+#This is a comment.
+print("Foysal Zihak")
+#print("Foysal Zihak")
+"""
+This is a comment
+written in
+more than just one line
+"""
+x = 500
+y = "Zihak"
+print(x)
+print(y)
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
