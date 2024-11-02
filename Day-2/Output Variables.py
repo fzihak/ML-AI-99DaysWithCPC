da3a25e39f60f2899cc4974fc22ebffadbb975cd
Foysal Zihak <67580891+fzihak@users.noreply.github.com>
2024-11-02 21:52:08 +0600

    Day-2  ml ai

---
+++ Day-2/Output Variables.py
@@ -0,0 +1,24 @@
+x = "Python is awesome"
+print(x)
+
+y = "Foysal Mahmud ZIhak"
+print(y)
+
+x = "Foysal"
+y = "Mahmud"
+z = "Zihak"
+print(x, y, z)
+
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = "John"
+print(x, y)
